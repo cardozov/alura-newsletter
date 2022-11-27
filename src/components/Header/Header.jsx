1f52c06f2435229136ad1d456a67617798061a66
cardozov <vinicius.gcardozo@gmail.com>
2022-11-27 11:47:05 -0300

    aula 2.2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import { MoonIcon } from "@heroicons/react/24/solid"
+import ToggleTheme from "../ToggleTheme/ToggleTheme"
 
 const Header = () => {
   return <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-    <span className="text-gray-100">Olá, usuário</span>
+    <span className="text-gray-100">Olá, Usuário</span>
     <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-    <MoonIcon className="h-8 text-gray-100 hidden sm:block cursor-pointer"/>
+    <ToggleTheme />
   </div>
 }
 
