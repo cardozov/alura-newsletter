269034ad6ffab6d558345f040e48eb5a6c98bebe
cardozov <vinicius.gcardozo@gmail.com>
2022-11-28 17:01:00 -0300

    aula 4.3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,5 @@
+import Input from "../Input/Input"
+
 const Form = ({ onSubmit }) => {
   const safeSubmit = event => {
     event.preventDefault()
@@ -7,10 +9,10 @@ const Form = ({ onSubmit }) => {
     onSubmit({ name, email })
   }
 
-  return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20">
-    <input type="text" placeholder="Insira seu nome..." />
-    <input type="email" placeholder="Insira seu e-mail..." />
-    <button type="submit" className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase">Seguir</button>
+  return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5">
+    <Input type="text" required placeholder="Insira seu nome..." />
+    <Input type="email" required placeholder="Insira seu e-mail..." />
+    <button type="submit" className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none">Seguir</button>
   </form>
 }
 
